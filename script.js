379f16ffd138cab9960cdaee1d8c2014edb02988
rohittz <r_o_h_i_t_roy@yahoo.com>
2021-02-09 14:04:37 +0600

    reponsiveness added

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
 let urlAllCountries = 'https://restcountries.eu/rest/v2/all';
 let divClassCountry, divClassimgName, imgFlag, countryNameh3, divCapitalCountry;
 let allTheCountries = document.getElementById("all-countries");
+
 async function getAllData(){
 	let rawData = await fetch(urlAllCountries);
 	let data = await rawData.json();
 	showData(data);
 }
 getAllData();
-function appendACountry(countryName, countryFlag, countryCapital){
-	createElements();
-	// appending a country class inside all-countries
-	allTheCountries.append(divClassCountry);
-	//appending classimgName inside this country
-	divClassCountry.style.background = `url("${countryFlag}")`;
-	divClassCountry.style.backgroundSize = "cover";
-	divClassCountry.append(divClassimgName);
-	// appending img inside classimgName
-	divClassimgName.append(imgFlag);
-	//putting the flag inside of items
-	imgFlag.setAttribute("src", countryFlag);
-	// append h3 inside classimgName
-	divClassimgName.append(countryNameh3);
-	countryNameh3.innerText = countryName;
-	// append divCapitalCountry inside country
-	divClassCountry.append(divCapitalCountry);
-	divCapitalCountry.innerText = countryCapital;
-}
 function showData(data){
 	let countryName, countryFlag, countryCapital;
-	for( let i = 0; i < data.length; i++){
-		countryName = data[i].name;
-		countryFlag = data[i].flag;
-		countryCapital = data[i].capital;
-		appendACountry(countryName, countryFlag, countryCapital);
-	}
+	data.forEach(country => {
+		countryName = country.name;
+		countryFlag = country.flag;
+		countryCapital = country.capital;
+		let aCountry = document.createElement("div");
+		aCountry.className = "country";
+		aCountry.innerHTML= `
+			<div class = "imgname">
+			<img src = "${countryFlag}" alt = "${countryName}">
+			<h3> ${countryName} </h3>
+			</div>
+			<div class = "capital">
+			${countryCapital}
+			</div>
+	`;
+		allTheCountries.append(aCountry);
+	});
 }
-function createElements(){
-	divClassCountry = document.createElement("div");
-	divClassCountry.setAttribute("class", "country");
-	divClassimgName = document.createElement("div");
-	divClassimgName.setAttribute("class", "imgname");
-	imgFlag = document.createElement("img");
-	countryNameh3 = document.createElement("h3");
-	divCapitalCountry = document.createElement("div");
-	divCapitalCountry.setAttribute("class", "capital");
 
-}
+// adding click event listener
+allTheCountries.addEventListener("click", function(event){
+	console.log(event);
+});
